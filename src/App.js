b45f16dc606c5251db8100e202bf3f1be093cbb5
iris paredes <irisparedesjimenez@gmail.com>
2019-05-24 09:11:06 +0200

    add basic structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <h1 className="title">Harry Potter Characters</h1>
+        <label htmlFor="input"></label>
+        <input type="text" className="input"/>
+        <ul className="list">
+          <li className="card__li">
+            <div className="card">
+              <div className="card__image">
+                <img src="" alt=""/>
+                <h2 className="name">Nombre Popoter</h2>
+                <h3 className="name__house">casita</h3>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     );
   }
