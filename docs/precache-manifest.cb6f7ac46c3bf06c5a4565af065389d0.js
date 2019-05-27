1f3a8f67dd7cb13185d39a164e3d6cbc8f2db919
iris paredes <irisparedesjimenez@gmail.com>
2019-05-27 15:06:22 +0200

    add docs folder

---
+++ docs/precache-manifest.cb6f7ac46c3bf06c5a4565af065389d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ffefe9a3c24703b1b81dee8010488e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e013f73b401420326396",
+    "url": "./static/css/main.5c8b72b4.chunk.css"
+  },
+  {
+    "revision": "9b844990af4e82fe98e6",
+    "url": "./static/js/2.bbb231b1.chunk.js"
+  },
+  {
+    "revision": "e013f73b401420326396",
+    "url": "./static/js/main.1f51e877.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b13ff2cbd1d98d884550803d02844648",
+    "url": "./static/media/gryffindor.b13ff2cb.png"
+  },
+  {
+    "revision": "e123d4268bc93f44bf4bdcd813c97ae3",
+    "url": "./static/media/hogwarts.e123d426.jpg"
+  },
+  {
+    "revision": "39dba15a5cba447ce049948ebb238149",
+    "url": "./static/media/hufflepuff.39dba15a.png"
+  },
+  {
+    "revision": "5e0f1a99f8ffc0d76ab3cba203591cd8",
+    "url": "./static/media/ravenclaw.5e0f1a99.png"
+  },
+  {
+    "revision": "7c46afc492099e94edd65b7250e6be7d",
+    "url": "./static/media/slytherin.7c46afc4.png"
+  }
+]);
